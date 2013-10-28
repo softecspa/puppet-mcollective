3adfd0627aaa9ac752a80fbb044802c8418df0d6
Andrea Cappelli <a.cappelli@gmail.com>
2013-10-28 17:36:50 +0100

    Install and configure agent and server

---
+++ manifests/agent.pp
@@ -0,0 +1,34 @@
+class mcollective::agent(
+  $ensure=present,
+  $stomp_host
+)
+{
+  if ! defined(Package['stomp']) {
+    package {'stomp':
+      ensure   => '1.2.2',
+      provider => gem,
+      before   => Package['mcollective-client'],
+    }
+  }
+
+  if ! defined (Package['mcollective-common']) {
+    apt::pin {
+      'mcollective-common': version => $mcollective::mcollective_version;
+    }
+  }
+
+  apt::pin {
+    'mcollective-client': version => $mcollective::mcollective_version;
+  } ->
+  package {
+    'mcollective-client': ensure => $mcollective::mcollective_version;
+  }
+
+  file { "mcollective-client.cfg":
+    path  => "/etc/mcollective/client.cfg",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    content => template('mcollective/client.cfg.erb')
+  }
+}
---
+++ manifests/init.pp
@@ -0,0 +1,63 @@
+class mcollective(
+  $ensure=present,
+  $stomp_host
+)
+{
+
+  $mcollective_version = '2.2.4-1'
+
+  if $ensure in [ 'present', 'running' ] {
+    $ensure_real = 'running'
+    $enable_real = true
+  } elsif $ensure in ['removed', 'stopped'] {
+    $ensure_real = 'stopped'
+    $enable_real = false
+  } else {
+    fail("ensure parameter must be present, running, removed or stopped, got: $ensure")
+  }
+
+  file{"/etc/mcollective/facts.yaml":
+    owner    => root,
+    group    => root,
+    mode     => 400,
+    loglevel => debug,
+    content  => inline_template("<%= scope.to_hash.reject { |k,v| k.to_s =~ /(uptime_seconds|timestamp|free)/ }.to_yaml %>"), # exclude rapidly changing facts
+    require => Package['mcollective'],
+  }
+
+  if ! defined(Package['stomp']) {
+    package {'stomp':
+      ensure   => '1.2.2',
+      provider => gem,
+      before   => Package['mcollective-client'],
+    }
+  }
+
+  apt::pin {
+    'mcollective':               version => $mcollective_version;
+    'mcollective-common':        version => $mcollective_version;
+  } ->
+  package {
+    'mcollective':               ensure => $mcollective_version;
+    'mcollective-common':        ensure => $mcollective_version;
+  }
+
+  file { "mcollective-server.cfg":
+    path  => "/etc/mcollective/server.cfg",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    content => template('mcollective/server.cfg.erb'),
+    require => Package['mcollective'],
+    notify => Service['mcollective']
+  }
+
+  service { 'mcollective':
+    ensure     => $ensure_real,
+    enable     => $enable_real,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => Package['mcollective'],
+  }
+
+}
